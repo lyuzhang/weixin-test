5b7f42f078e9098e83b4359899dc225799291935
zhangjiewei <1525456439@qq.com>
2018-11-20 09:28:08 +0800

    test

--- day01/04.获取access_token/config/config.js
+++ day01/04.获取access_token/config/config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   appID : 'wxe368516e4e517445',
-    appsecret :'9ca656b40ec0808dd8deff75bb194a93',
+  appsecret :'9ca656b40ec0808dd8deff75bb194a93',
   token:'lyuzhangfirstweixingongzhonghao'
 
 }
